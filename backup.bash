db626ce4d39fed2546875f4cb5891886f26a0e91
user <user@localhost.localdomain>
2020-12-23 19:40:07 +0300

    3 4

--- backup.bash
+++ backup.bash
@@ -7,7 +7,13 @@ lastBackup=$(ls "$HOME" | grep -e "^Backup-[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$" | 
 date2=$(echo $lastBackup | cut -c "8-17")
 date2InSec=$(date -d "date2" +"%s")
 rangeDate=$(echo ($dateInSec-$date2InSec)/24/60/60 | bc)
-if [[ $rangeDate -ge 7 ]]
+
+if [[ $rangeDate -lt 7 ]]
+then
+    flag=1
+fi
+
+if [[ $lastBackup == "" ]]
 then
     flag=0
 fi
@@ -18,9 +24,30 @@ then
     echo "Сделан новый backup: Backup-$DATE" >> $HOME/backup-report
     for line in ($ls "$HOME/source")
     do
-	cp -f "$HOME/source/$line" "$HOME/Backup-$DATE"
+	cp "$HOME/source/$line" "$HOME/Backup-$DATE"
 	echo "$line" >> $HOME/backup-report
     done
+    echo "" >> $HOME/backup-report
 else
     cd $HOME/$lastBackup
+    echo "Список изменений:" >> temp
+    for line in $(ls)
+    do
+	if ! [[ -f "$HOME/$lastBackup/$line" ]]
+	then
+	    cp "$HOME/source/$line" "HOME/$lastBackup"
+	    echo "Добавлен $line" >> temp
+	else
+	    oldSize=$(wc -c "$HOME/$lastBackup/$line" | awk '{print $1}')
+	    newSize=$(wc -c "$HOME/$lastBackup/$line" | awk '{print $1}')
+	    if [[ "$newSize" -ne "$oldSize" ]]
+	    then
+		mv "$HOME/$lastBackup/$line" "$HOME/$lastBackup/$line.$date2"
+		echo "Именён $line" >> temp
+		cp "$HOME/source/$line" "$HOME/$lastBackup/$line"
+	    fi
+	fi
+    done
+    cat temp >> $HOME/$lastBackup
+    rm temp
 fi
--- upback.bash
+++ upback.bash
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+if [[ -d "$HOME/restore" ]]
+then
+    rmdir "/$HOME/restore"
+fi
+mkdir "$HOME/restore"
+lastBackup=$(ls $HOME | grep -e "^Backup-[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$" | sort -n | tail -1)
+if [[ $lastBackup == "" ]]
+then
+    echo "Backup не существует"
+    exit 1
+fi
+for line in $(ls "$HOME/lastBackup" | grep -ev ".[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}$" )
+do
+    cp "$HOME/$lastBackup/$line" "$HOME/$restore/$line"
+done
